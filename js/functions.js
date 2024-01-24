d921297ea0f43f2b1ea86aac2d184c47b0d61913
Your Name <you@example.com>
2024-01-25 01:45:18 +0300

    first commit

---
+++ js/functions.js
@@ -0,0 +1,115 @@
+$(document).ready(function(){
+  $('.orientation_slider_card-content').matchHeight({
+    byRow: false,
+  });
+  $('.qual_slider_card-wrapper').matchHeight();
+
+  $('.orientation_slider').slick({
+        slidesToShow: 4,
+        dots: true,
+        dotsClass: 'slider_dots',
+        arrows: true,
+        prevArrow: '<div class="slider_arrow slider_arrowleft"></div>',
+        nextArrow: '<div class="slider_arrow slider_arrowright"></div>',
+        responsive: [
+    {
+      breakpoint: 993,
+      settings: {
+        slidesToShow: 3,
+      }
+    },  
+    {
+      breakpoint: 556,
+      settings: {
+        slidesToShow: 1,
+      }
+    },  
+  ],
+    });
+
+  $('.qual_slider').slick({
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3,
+        dots: true,
+        dotsClass: 'slider_dots',
+        arrows: true,
+        prevArrow: '<div class="slider_arrow slider_arrowleft"></div>',
+        nextArrow: '<div class="slider_arrow slider_arrowright"></div>',
+        responsive: [
+    {
+      breakpoint: 556,
+      settings: {
+        slidesToShow: 1,
+        centerPadding: '20px',
+      }
+    },  
+  ],
+
+    });
+
+  $('.review_slider').slick({
+        slidesToShow: 4,
+        dots: true,
+        dotsClass: 'slider_dots',
+        arrows: true,
+        prevArrow: '<div class="slider_arrow slider_arrowleft"></div>',
+        nextArrow: '<div class="slider_arrow slider_arrowright"></div>',
+        responsive: [
+    {
+      breakpoint: 556,
+      settings: {
+        slidesToShow: 1,
+      }
+    },  
+  ],
+    });
+
+  $('a[href="#aboutMe"]').click(function (){
+    if ($('.burger-menu-wrapper').hasClass('burger-display')){
+      $('.burger-menu-wrapper').toggleClass('burger-display')
+    };
+    $('html, body').animate({
+      scrollTop: $('.head').offset().top
+    }, 1000);
+  });
+
+  $('a[href="#aboutPoject"]').click(function (){
+    if ($('.burger-menu-wrapper').hasClass('burger-display')){
+      $('.burger-menu-wrapper').toggleClass('burger-display')
+    };
+    $('html, body').animate({
+      scrollTop: $('.orientation').offset().top
+    }, 1000);
+  });
+
+  $('a[href="#format"]').click(function (){
+    if ($('.burger-menu-wrapper').hasClass('burger-display')){
+      $('.burger-menu-wrapper').toggleClass('burger-display')
+    };
+    $('html, body').animate({
+      scrollTop: $('.format').offset().top
+    }, 1000);
+  });
+
+  $('a[href="#review"]').click(function (){
+    if ($('.burger-menu-wrapper').hasClass('burger-display')){
+      $('.burger-menu-wrapper').toggleClass('burger-display')
+    };
+    $('html, body').animate({
+      scrollTop: $('.review').offset().top
+    }, 1000);
+  });
+
+  $('#burger-close').click(function() {
+      $('.burger-menu-wrapper').toggleClass('burger-display')
+  });
+  $('.burger-button').click(function() {
+      $('.burger-menu-wrapper').toggleClass('burger-display')
+  });
+
+});
+
+
+
+
